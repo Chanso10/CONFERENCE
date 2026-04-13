2255d1218d1d7f7c940b979921770181104d5974
Joel Osterhus <joel.osterhus@gmail.com>
2026-04-13 17:12:23 -0500

    db.js now uses .env for easier changes to ports and stuff

--- BCONF/server/db.js
+++ BCONF/server/db.js
@@ -1,10 +1,14 @@
+const dotenv = require("dotenv");
 const Pool = require("pg").Pool;
-const pool= new Pool({
-    user:"bconf",
-    password:"g0valp0",
-    host:"localhost",
-    port:5432,
-    database:"bconf"
+
+dotenv.config();
+
+const pool = new Pool({
+    user: process.env.DB_USER || "bconf",
+    password: process.env.DB_PASSWORD || "g0valp0",
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    database: process.env.DB_NAME || "bconf"
 });
 
 module.exports = pool;
