6ebe727fff0d773f9e5d7e9b99904551e4b08e4b
Joel Osterhus <joel.osterhus@gmail.com>
2026-04-20 15:18:58 -0500

    frontend port now editable within .env. Also PERN-TODO folder has been slimed out

--- PERN-TODO/server/database.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS papers;
-
-CREATE TABLE papers(
-  paper_id SERIAL PRIMARY KEY,
-  author VARCHAR(255),
-  description VARCHAR(255),
-  pdf_path VARCHAR(255),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
